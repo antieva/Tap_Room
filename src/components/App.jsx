ccb99a326e89e20349a7b82c9279e61bbcf2f1d6
Eva Antipina <eva.antipina@gmail.com>
2018-06-29 12:31:49 -0700

    Add router.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import BeerList from './BeerList';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
@@ -6,6 +8,9 @@ function App(){
       <h1>Tap Room</h1>
       <h3>Current beer:</h3>
       <hr/>
+        <Switch>
+          <Route exact path='/' component={BeerList} />
+        </Switch>
     </div>
   );
 }
---
+++ src/components/BeerList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function BeerList() {
+  return (
+    <div>
+      <h3>Beer name</h3>
+      <ul>
+        <li>brewer</li>
+        <li>description</li>
+        <li>abv</li>
+        <li>price</li>
+        <li>remaining</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BeerList;
