da09ea79156412dd13fcf7de18d884b333058f3b
Eva Antipina <eva.antipina@gmail.com>
2018-06-29 16:00:03 -0700

    Add Error404 component and route to that page which is any non-existant path.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import BeerList from './BeerList';
 import { Switch, Route } from 'react-router-dom';
 import NewBeerForm from './NewBeerForm';
+import Error404 from './Error404';
 
 function App(){
   return (
@@ -12,6 +13,7 @@ function App(){
         <Switch>
           <Route exact path='/' component={BeerList} />
           <Route path='/newbeer' component={NewBeerForm} />
+          <Route component={Error404} />
         </Switch>
     </div>
   );
